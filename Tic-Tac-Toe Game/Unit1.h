455aac69f05afae8c942b2fac37e577b8250a038
Thinh <thinhpt00@gmail.com>
2023-02-18 17:56:30 +0700

    Tic Tac Toe Game

---
+++ Tic-Tac-Toe Game/Unit1.h
@@ -0,0 +1,37 @@
+//---------------------------------------------------------------------------
+
+#ifndef Unit1H
+#define Unit1H
+//---------------------------------------------------------------------------
+#include <System.Classes.hpp>
+#include <FMX.Controls.hpp>
+#include <FMX.Forms.hpp>
+#include <FMX.Controls.Presentation.hpp>
+#include <FMX.StdCtrls.hpp>
+#include <FMX.Types.hpp>
+//---------------------------------------------------------------------------
+class TForm1 : public TForm
+{
+__published:	// IDE-managed Components
+	TButton *Button1;
+	TButton *Button2;
+	TButton *Button3;
+	TButton *Button4;
+	TButton *Button5;
+	TButton *Button6;
+	TButton *Button7;
+	TButton *Button8;
+	TButton *Button9;
+	TLabel *Label1;
+	TLabel *playerLabel;
+	TLabel *Label2;
+	void __fastcall ButtonClick(TObject *Sender);
+private:	// User declarations
+    bool TForm1::isPlayerWin();
+public:		// User declarations
+	__fastcall TForm1(TComponent* Owner);
+};
+//---------------------------------------------------------------------------
+extern PACKAGE TForm1 *Form1;
+//---------------------------------------------------------------------------
+#endif
